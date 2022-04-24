ce0f20f2390294dddbef3970c0077b3d1aab25dc
Tim M <timothy.romero.manas@gmail.com>
2022-04-23 23:52:41 -0400

    Adding Secondary runner

---
+++ src/test/java/runners/SecondaryRunner.java
@@ -0,0 +1,26 @@
+package runners;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class SecondaryRunner {
+	public static void main(String[] args) {
+        // Creating a new instance of the HTML unit driver
+         
+        WebDriver driver = new HtmlUnitDriver();
+         
+		     // Navigate to Google		
+//        driver.get("https://marketchameleon.com/Reports/PremarketTrading");					
+        driver.get("https://marketchameleon.com/Reports/PremarketTrading");					
+
+        
+		// This code will print the page source Code		
+       System.out.println("Page title is: " + driver.getPageSource());		
+       
+       driver.quit();			
+}	
+	
+
+}
